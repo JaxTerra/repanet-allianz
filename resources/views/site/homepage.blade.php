29ed94e66dc779062850b964a7d3234d8e1cc224
JaxTerra <jax@beitsafe.com>
2021-04-21 13:02:59 +1000

    Draft 1 - ammend heading

--- resources/views/site/homepage.blade.php
+++ resources/views/site/homepage.blade.php
@@ -83,6 +83,12 @@
     <section class="text-center bg--dark">
         <div class="container">
             <div class="row">
+                <div class="col-md-12">
+                    <h2> Our Services</h2>
+                </div>
+            </div>
+
+            <div class="row">
                 <div class="col-md-6">
                     <div class="feature feature-3 boxed boxed--lg boxed--border">
                         <i class="icon icon--lg icon-Mail-3"></i>
@@ -194,11 +200,30 @@
             <div class="row justify-content-center">
                 <div class="col-md-8 col-lg-7 boxed boxed--border">
                     <h3 class="mb-0">Contact Us</h3>
-                    <form class="text-left form-email row" data-success="Thanks for your enquiry, we'll be in touch shortly." data-error="Please fill in all fields correctly.">
-                        <div class="col-md-6"> <label>Your Name:</label> <input type="text" name="Name" class="validate-required"> </div>
-                        <div class="col-md-6"> <label>Email Address:</label> <input type="email" name="email" class="validate-required validate-email"> </div>
-                        <div class="col-md-12"> <label>Message:</label> <textarea rows="6" name="Message" class="validate-required"></textarea> </div>
-                        <div class="col-md-5 col-lg-4"> <button type="submit" class="btn btn--primary type--uppercase">Send Enquiry</button> </div>
+                    <form class="text-left form-email row mx-0"
+                          data-success="Thanks for your enquiry, we'll be in touch shortly."
+                          data-error="Please fill in all fields correctly."
+                          method="POST"
+                          action="{{url('contact-send')}}">
+                        @csrf
+                        <div class="col-md-6">
+                            <label class="type--bold">Your Name:</label>
+                            <input type="text" name="name" class="validate-required">
+                        </div>
+                        <div class="col-md-6">
+                            <label class=" type--bold">Email Address:</label>
+                            <input type="email" name="email" class="validate-required validate-email">
+                        </div>
+                        <div class="col-md-12">
+                            <label class=" type--bold">Message:</label>
+                            <textarea rows="6" name="message" class="validate-required"></textarea>
+                        </div>
+                        <div class="col-md-6 col-xs-12 mt--1">
+                            @captcha
+                        </div>
+                        <div class="col-md-6 col-xs-12 pull-right mt--1">
+                            <button type="submit" class="btn btn--primary type--uppercase">Send Enquiry</button>
+                        </div>
                     </form>
                 </div>
             </div>
