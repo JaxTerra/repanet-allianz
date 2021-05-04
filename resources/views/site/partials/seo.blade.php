60f5d5c242e0cf7e99741c6168e8ba4d9a4659a0
JaxTerra <jax@beitsafe.com>
2021-05-04 15:40:24 +1000

    Draft 1 - SEO updates

--- resources/views/site/partials/seo.blade.php
+++ resources/views/site/partials/seo.blade.php
@@ -1,13 +1,17 @@
 <!-- Meta Data -->
-<meta name="description" content="">
-<meta name="keywords" content="">
-<meta name="author" content="Oz Window Films">
-<meta name="publisher" content="Oz Window Films" href="{{url('https://repanet.com.au')}}">
+<meta name="description" content="REPANET is a repair network of licensed trades all committed to providing solutions,
+managed under one company. Specialists in storm response management and repairs from years of experience of working with
+clients and insurance companies alike.">
+<meta name="keywords" content="licensed trades, repanet, repair, repairs, repair network, storm, storms, storm response management, specialists, insurance, insurance repairs">
+<meta name="author" content="Repanet">
+<meta name="publisher" content="Repanet" href="{{url('https://repanet.com.au')}}">
 <meta name="robots" content="index, follow">
 
 
-<meta property="og:title" content=""/>
-<meta property="og:description" content=""/>
+<meta property="og:title" content="REPANET"/>
+<meta property="og:description" content="REPANET is a repair network of licensed trades all committed to providing
+solutions, managed under one company. Specialists in storm response management and repairs from years of experience of
+working with clients and insurance companies alike."/>
 <meta property="og:type" content="article" />
 <meta property="og:url" content="{{ url()->current() }}"/>
 <meta property="og:image" content="{{asset('logos/repanet_seo_branding_image.png')}}" />
