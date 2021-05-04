499009e434f677f8cfefd5c29b8a3671ce64d871
JaxTerra <jax@beitsafe.com>
2021-05-04 22:19:09 +1000

    Draft 1 - SEO update

--- resources/views/site/partials/seo.blade.php
+++ resources/views/site/partials/seo.blade.php
@@ -5,6 +5,7 @@ clients and insurance companies alike.">
 <meta name="keywords" content="licensed trades, repanet, repair, repairs, repair network, storm, storms, storm response management, specialists, insurance, insurance repairs">
 <meta name="author" content="Repanet">
 <meta name="publisher" content="Repanet" href="{{url('https://repanet.com.au')}}">
+<meta name="image" content="{{asset('logos/repanet_seo_branding_image.png')}}" />
 <meta name="robots" content="index, follow">
 
 
