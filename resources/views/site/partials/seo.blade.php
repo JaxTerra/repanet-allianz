2f0455f082b0bcf21dacda5f9a97bafe39d1358c
JaxTerra <jax@beitsafe.com>
2021-05-05 12:29:36 +1000

    Draft 1 - SEO update

--- resources/views/site/partials/seo.blade.php
+++ resources/views/site/partials/seo.blade.php
@@ -1,7 +1,5 @@
 <!-- Meta Data -->
-<meta name="description" content="REPANET is a repair network of licensed trades all committed to providing solutions,
-managed under one company. Specialists in storm response management and repairs from years of experience of working with
-clients and insurance companies alike.">
+<meta name="description" content="REPANET is a repair network of licensed trades all committed to providing solutions, managed under one company.">
 <meta name="keywords" content="licensed trades, repanet, repair, repairs, repair network, storm, storms, storm response management, specialists, insurance, insurance repairs">
 <meta name="author" content="Repanet">
 <meta name="publisher" content="Repanet" href="{{url('https://repanet.com.au')}}">
@@ -10,9 +8,8 @@ clients and insurance companies alike.">
 
 
 <meta property="og:title" content="REPANET"/>
-<meta property="og:description" content="REPANET is a repair network of licensed trades all committed to providing
-solutions, managed under one company. Specialists in storm response management and repairs from years of experience of
-working with clients and insurance companies alike."/>
+<meta property="og:description" content="REPANET is a repair network of licensed trades all committed to providing solutions, managed under one company."/>
+<meta name="og:keywords" content="licensed trades, repanet, repair, repairs, repair network, storm, storms, storm response management, specialists, insurance, insurance repairs">
 <meta property="og:type" content="article" />
 <meta property="og:url" content="{{ url()->current() }}"/>
 <meta property="og:image" content="{{asset('logos/repanet_seo_branding_image.png')}}" />
